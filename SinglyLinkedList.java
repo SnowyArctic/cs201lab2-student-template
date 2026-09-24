a8d1aec20e4758284f7b761894d3a7f73772de15
SnowyArctic <john.liu.2025@computing.smu.edu.sg>
2026-09-24 16:36:54 +0800

    Submission 1 Lab_2

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -8,63 +8,63 @@ public class SinglyLinkedList<E extends Comparable<E>> {
     private static class Node<E> {
         private E element;
         private Node<E> next;
-    
-        public Node(E e, Node<E> n){
+
+        public Node(E e, Node<E> n) {
             element = e;
             next = n;
         }
-    
-        public E getElement(){
+
+        public E getElement() {
             return element;
         }
-    
-        public Node<E> getNext(){
+
+        public Node<E> getNext() {
             return next;
         }
-    
-        public void setNext(Node<E> n){
+
+        public void setNext(Node<E> n) {
             next = n;
         }
     }
 
-    public SinglyLinkedList(){
+    public SinglyLinkedList() {
 
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public E first(){
-        if (isEmpty()){
+    public E first() {
+        if (isEmpty()) {
             return null;
-        } 
+        }
         return head.getElement();
     }
 
-    public E last(){
-        if (isEmpty()){
+    public E last() {
+        if (isEmpty()) {
             return null;
         }
         return tail.getElement();
     }
 
-    public void addFirst(E e){
+    public void addFirst(E e) {
         head = new Node<>(e, head);
 
-        if (isEmpty()){
+        if (isEmpty()) {
             tail = head;
         }
         size++;
     }
 
-    public void addLast(E e){
+    public void addLast(E e) {
         Node<E> newest = new Node<>(e, null);
-        if (isEmpty()){
+        if (isEmpty()) {
             head = newest;
         } else {
             tail.setNext(newest);
@@ -73,8 +73,8 @@ public class SinglyLinkedList<E extends Comparable<E>> {
         size++;
     }
 
-    public E removeFirst(){
-        if (isEmpty()){
+    public E removeFirst() {
+        if (isEmpty()) {
             return null;
         }
 
@@ -82,13 +82,13 @@ public class SinglyLinkedList<E extends Comparable<E>> {
         head = head.getNext();
         size--;
 
-        if (isEmpty()){
+        if (isEmpty()) {
             tail = null;
         }
         return answer;
     }
 
-    public String toString(){
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         Node<E> current = head;
         while (current != null) {
@@ -100,10 +100,49 @@ public class SinglyLinkedList<E extends Comparable<E>> {
     }
 
     // write your codes here
-    public void swap(){
-        
+    public void swap() {
+        if (isEmpty()) {
+            return;
+        }
 
+        Node<E> current = head;
+
+        List<Node<E>> originalOrder = new ArrayList<>();
+        while (current != null) {
+            originalOrder.add(current);
+            current = current.getNext();
+        }
+
+        List<Node<E>> sortedOrder = new ArrayList<>(originalOrder);
+
+        sortedOrder.sort(new Comparator<Node<E>>() {
+            @Override
+            public int compare(Node<E> node1, Node<E> node2) {
+                return node1.getElement().compareTo(node2.getElement());
+            }
+        });
+
+        int n = originalOrder.size();
+        for (int i = 0; i < n / 2; i++) {
+            Node<E> forward = sortedOrder.get(i);
+            Node<E> backward = sortedOrder.get(n - 1 - i);
+
+            int indexForward = originalOrder.indexOf(forward);
+            int indexBackward = originalOrder.indexOf(backward);
+
+            Collections.swap(originalOrder, indexForward, indexBackward);
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            Node<E> present = originalOrder.get(i);
+            Node<E> next = originalOrder.get(i + 1);
+
+            present.setNext(next);
+        }
+
+        head = originalOrder.get(0);
+        tail = originalOrder.get(n-1);
+
+        tail.setNext(null);
     }
-   
 }
-
